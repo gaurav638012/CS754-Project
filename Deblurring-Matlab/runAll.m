b8083db2d52ac35c3e941ecc6a8540254e0624fe
gaurav638012 <gaurav638012@gmail.com>
2021-05-10 20:46:40 +0530

    some changes

--- Deblurring-Matlab/runAll.m
+++ Deblurring-Matlab/runAll.m
@@ -4,7 +4,7 @@ clc;
 
 str = "kernel,image,peak,PSNR\n";
 
-for kernel_type = ["gaussian"]
+for kernel_type = ["custom"]
     for image = ["sat","pepper","cameraman","ridges","house","curve"]
         for peak = [1]
             [rec, PSNR] = deblur(sprintf("images/%s.png",image),peak,kernel_type,0);
--- DenoisingMatlab/loading.m
+++ DenoisingMatlab/loading.m
@@ -2,5 +2,6 @@ function [org_img,noised_img] = loading(filename,peak)
 org_img = im2double(imread(filename));
 org_img = rgb2gray(org_img);
 noised_img=poissrnd(org_img*peak);
+
 end
 
